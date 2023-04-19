aede21cb5f0a8d76f370ead481775f74a35359f2
Alejandro lucero <realslyfer@gmail.com>
2023-04-19 05:29:19 +0000

    se cambio a timestamp

--- contract/tictactoe.cpp
+++ contract/tictactoe.cpp
@@ -76,15 +76,10 @@ CONTRACT tictactoe:public contract{
 	TABLE games_record{
 		name host;
 		name challenger;
-		time_point timestamp;
+		uint64_t timestamp;//cambiado
 		uint64_t primary_key() const{ return host.value; }
-		
-		//solucion por chatGPT
-		//uint64_t combinat() const {return combine_ids(host.value, challenger.value);}
-		uint64_t combinat() const {
- 			 std::string combined_key = std::to_string(host.value) + std::to_string(challenger.value);
-  				return std::stoull(combined_key);
-		}
+		uint64_t by_secondary() const{ return challenger.value; }
+
 
 		//esto era para serializar la estructura:
 		EOSLIB_SERIALIZE(games_record, (host)(challenger)(timestamp))
@@ -97,7 +92,7 @@ CONTRACT tictactoe:public contract{
 	Y el nombre abstracto de nuestro tipo se llama srv index
 	*/
 	
-	typedef multi_index<name("games"), games_record, indexed_by<name("hostchall"), const_mem_fun<games_record, uint64_t, &games_record::combinat>>> games_table;
+	typedef multi_index<name("games"), games_record, indexed_by<name("timestamps"), const_mem_fun<games_record, uint64_t, &games_record::by_secondary>>> games_index;
 
 	/*====	creando la instancia: ====*/
 	//el nombre de usuario se guardara en nuestra tabla de indices multiples:
@@ -106,16 +101,24 @@ CONTRACT tictactoe:public contract{
 		require_auth(get_self());
 		
 		//chequeo si un guardado con el mismo host y challenger ya existe
-		games_table games(get_self(), get_self().value);
+		games_index games(get_self(), get_self().value);
 
 		/*al realizar push action nuevamente, se genera un error, porque cleos nos da un indice repetido en la tabla de indices multiples, 
 			por lo tanto hay que hacer que la funcion ask verifique si el juego ya fue incluido en la tabla. Primero hay que buscar el registro
 			Ahora usamos find: devuelve un iterador*/
 
-		auto combinat_index = games.get_index<name("hostchall")>();
-		auto existing_game = combinat_index.find(combinat()/*combine_ids(host.value, challenger.value)*/);
-		check(existing_game == combinat_index.end(), "Este juego ya existe");
-		
+		auto idx = games.get_index<name("timestamps")>();
+		for(auto itr = idx.begin(); itr != idx.end(); itr++) {
+			eosio::print_f("Registro del acceso de usuario: ", itr->host, itr->timestamp);
+			eosio::print_f("Registro del acceso del retador: ", itr->challenger, itr->timestamp);
+		}
+
+		//si el indice secundario tenia un nombre
+		itr = idx.find("Ale"); //se puede usar idx.upper_bound(timestamp) o lower_bound para buscar los primeros o ultimos registros por timestamps
+
+
+
+				
 		//Create a new game record
 		games.emplace(get_self(), [&](auto& new_game){
 			new_game.host = host;
@@ -128,15 +131,12 @@ CONTRACT tictactoe:public contract{
 	ACTION close(name host, name challenger){
 		//verifico que juego existe en la tabla
 		games_table games(get_self(), get_self().value);
-		auto combinat_index = games.get_index<name("hostchall")>();
-		auto existing_game = combinat_index.find(combinat()/*combine_ids(host.value, challenger.value)*/);
-		check(existing_game != combinat_index.end(), "Este juego no existe");
+		
 		
 		//verifico que esa accion se llamo por el host o challenger
 		check(has_auth(host) || has_auth(challenger), "Solo el host o el challenger pueden cerrar el juego");
 
 		//Uso erase para borrar el registro de la tabla games
-		combinat_index.erase(existing_game);
 		
 	}
 	
